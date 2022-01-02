2fc3fa5078cc9aa92c2f4fecd3226ddef5f94f7a
Jeroen van der Schaaf <j.vanderschaaf@cmcgroep.nl>
2022-01-02 15:58:26 +0100

    rfid + weight to mqqt

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -8,11 +8,12 @@
 #include "RFID.h"
 
 // Pin Definitions
-#define SCALE_PIN_DAT 3
-#define SCALE_PIN_CLK 2
 
-#define RFID_PIN_RST 4
-#define RFID_PIN_SDA 5
+#define SCALE_PIN_DAT A2
+#define SCALE_PIN_CLK A1
+
+#define RFID_PIN_RST 6
+#define RFID_PIN_SDA 7
 
 // object initialization
 HX711 scale(SCALE_PIN_DAT, SCALE_PIN_CLK);
@@ -21,6 +22,12 @@ RFID rfid(RFID_PIN_SDA, RFID_PIN_RST);
 
 const int timeout = 10000;
 const int calibration_factor = 32600;
+const float minimum_weight_treshold = 0.05;
+
+float currentWeight = 0.00;
+constexpr size_t BUFFER_SIZE = 7; //1 char for the sign, 1 char for the decimal dot, 4 chars for the value & 1 char for null termination
+char buffer[BUFFER_SIZE];
+char rfid_buffer[100];
 
 uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x06};
 
@@ -48,6 +55,7 @@ PubSubClient client(server, 1883, callback, ethClient, sram);
 
 void setup()
 {
+
     Serial.begin(9600);
     Serial.println("Starting..");
 
@@ -61,8 +69,7 @@ void setup()
 
     if (client.connect("arduinoClient"))
     {
-        client.publish("outTopic", "hello world");
-        client.subscribe("inTopic");
+        Serial.println("Connected with MQTT Broker");
     }
 
     sram.begin();
@@ -83,18 +90,30 @@ void setup()
 
 void loop()
 {
-    float scaleUnits = scale.get_units();
-    Serial.print(scaleUnits);
-    Serial.println(" Kg");
-
-    delay(100);
-
-    constexpr size_t BUFFER_SIZE = 7; //1 char for the sign, 1 char for the decimal dot, 4 chars for the value & 1 char for null termination
-    char buffer[BUFFER_SIZE];
-    dtostrf(scaleUnits, BUFFER_SIZE - 1 /*width, including the decimal dot and minus sign*/, 2 /*precision*/, buffer);
-    client.publish("home/catbert/currentWeight", buffer, BUFFER_SIZE); //notice we're using the overload where you specify the length of the buffer, as we know it and it saves a call to strlen
     client.loop();
+    float scaleUnits = scale.get_units();
+    String rfidUid = "";
+
+    if ((currentWeight - scaleUnits) < -minimum_weight_treshold || (currentWeight - scaleUnits > minimum_weight_treshold))
+    {
+        Serial.print(scaleUnits);
+        Serial.println(" Kg");
+        currentWeight = scaleUnits;
+
+        dtostrf(currentWeight, BUFFER_SIZE - 1 /*width, including the decimal dot and minus sign*/, 2 /*precision*/, buffer);
+        client.publish("home/catbert/currentWeight", buffer, BUFFER_SIZE);
+    }
 
     String rfidtag = rfid.readTag();
-    rfid.printTag(rfidtag);
+
+    if (rfidtag != "None")
+    {
+        Serial.println(rfidtag);
+        rfidtag.toCharArray(rfid_buffer, 11);
+        // dtostrf(rfidtag, BUFFER_SIZE - 1 /*width, including the decimal dot and minus sign*/, 2 /*precision*/, buffer);
+        // client.publish("home/catbert/currentRDIF", String.toCharArray(rfidtag), BUFFER_SIZE);
+        client.publish("home/catbert/currentRFID", rfid_buffer);
+    }
+
+    delay(100);
 }
