185b4132bc55fda0bd6d00a848790df30630563f
jfvanderschaaf <j.vanderschaaf@gmail.com>
2022-01-05 19:19:07 +0100

    minor changes hardware

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -14,13 +14,13 @@
 #define SCALE_PIN_CLK A1
 
 #define RFID_PIN_RST 6
-#define RFID_PIN_SDA 7
+#define RFID_PIN_SDA 5
 
 // object initialization
 HX711 scale(SCALE_PIN_DAT, SCALE_PIN_CLK);
 EthernetClient ethClient;
 RFID rfid(RFID_PIN_SDA, RFID_PIN_RST);
-LiquidCrystal595 lcd(3, 2, 4);
+LiquidCrystal595 lcd(7, 8, 9);
 
 const int timeout = 10000;
 const int calibration_factor = 32600;
@@ -33,7 +33,7 @@ char rfid_buffer[100];
 
 uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x06};
 
-// MQTT server
+// // MQTT server
 IPAddress server(192, 168, 178, 10);
 
 SRAM sram(4, SRAM_1024);
@@ -57,7 +57,6 @@ PubSubClient client(server, 1883, callback, ethClient, sram);
 
 void setup()
 {
-
     Serial.begin(9600);
     Serial.println("Starting..");
 
@@ -86,19 +85,24 @@ void setup()
     scale.set_scale(calibration_factor);
     scale.tare();
 
-    // RFID
+    // // RFID
     rfid.init();
 
     // LCD
     lcd.begin(16, 2); // 16 characters, 2 rows
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("IP: " + Ethernet.localIP());
+    delay(1000);
 }
 
 void loop()
 {
+
     client.loop();
     float scaleUnits = scale.get_units();
     String rfidUid = "";
-
+    Serial.println(scaleUnits);
     if ((currentWeight - scaleUnits) < -minimum_weight_treshold || (currentWeight - scaleUnits > minimum_weight_treshold))
     {
         Serial.print(scaleUnits);
@@ -107,24 +111,29 @@ void loop()
 
         dtostrf(currentWeight, BUFFER_SIZE - 1 /*width, including the decimal dot and minus sign*/, 2 /*precision*/, buffer);
         client.publish("home/catbert/currentWeight", buffer, BUFFER_SIZE);
+
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(scaleUnits);
     }
 
     String rfidtag = rfid.readTag();
 
+    rfid.printTag(rfidtag);
     if (rfidtag != "None")
     {
         Serial.println(rfidtag);
         rfidtag.toCharArray(rfid_buffer, 11);
         // dtostrf(rfidtag, BUFFER_SIZE - 1 /*width, including the decimal dot and minus sign*/, 2 /*precision*/, buffer);
         // client.publish("home/catbert/currentRDIF", String.toCharArray(rfidtag), BUFFER_SIZE);
-        client.publish("home/catbert/currentRFID", rfid_buffer);
-    }
+        // client.publish("home/catbert/currentRFID", rfid_buffer);
 
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Wow. 3 pins!");
-    lcd.setCursor(0, 1);
-    lcd.print("Fabulous");
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Huidige RFID:");
+        lcd.setCursor(0, 1);
+        lcd.print(rfidtag);
+    }
 
     delay(100);
 }
