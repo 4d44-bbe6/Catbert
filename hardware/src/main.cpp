5b6102314ee26cc3e8662aafd949902d78dace0d
Jeroen van der Schaaf <j.vanderschaaf@cmcgroep.nl>
2022-03-03 15:17:24 +0100

    hardware changes - pins

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -15,25 +15,25 @@
 
 // Pin definitions
 
-#define SCALE_PIN_DAT A2
-#define SCALE_PIN_CLK A1
+#define SCALE_PIN_DAT A1
+#define SCALE_PIN_CLK A2
 
-#define RFID_PIN_RST 6
-#define RFID_PIN_SDA 5
+#define RFID_PIN_RST 2
+#define RFID_PIN_SDA 3
 
 // Object initialization
-// EthernetClient ethClient;
+EthernetClient ethClient;
 RFID rfid(RFID_PIN_SDA, RFID_PIN_RST);
 LiquidCrystal595 lcd(7, 8, 9);
 HX711 scale(SCALE_PIN_DAT, SCALE_PIN_CLK);
 
 // Timing
-static const unsigned long REFRESH_INTERVAL = 5000;
+static const unsigned long REFRESH_INTERVAL = 500;
 static unsigned long lastReconnectAttempt = 0;
 static unsigned long lastDataSent = 0;
 
 // Scale variables
-const int timeout = 10000;
+const int timeout = 1000;
 const int calibration_factor = 1265;
 const float minimum_weight_treshold = 0.05;
 
@@ -42,16 +42,16 @@ float prevWeight = 0.00;
 // MQTT variables
 constexpr size_t BUFFER_SIZE = 7; // 1 char for the sign, 1 char for the decimal dot, 4 chars for the value & 1 char for null termination
 char buffer[BUFFER_SIZE];
-// PubSubClient client(ethClient);
+PubSubClient client(ethClient);
 
 // RFID variables
 char rfid_buffer[100];
-String rfidtag = "";
-String prevrfidTag = "";
+String rfidtag;
+String prevrfidTag;
 
 // Ethernet variables
-// uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x06};
-// IPAddress mqtt_server(192, 168, 178, 10);
+uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x06};
+IPAddress mqtt_server(192, 168, 178, 10);
 
 // Misc variables
 String scaleName = "Catbert-001";
@@ -94,7 +94,7 @@ void publishWeight(float weight)
         if (weight - prevWeight > 5.00 || prevWeight - weight > 5.00)
         {
             dtostrf(weight, BUFFER_SIZE - 1 /*width, including the decimal dot and minus sign*/, 2 /*precision*/, buffer);
-            // client.publish("home/catbert/scales/Scale001/currentWeight", buffer, BUFFER_SIZE);
+            client.publish("home/catbert/scales/Scale001/currentWeight", buffer, BUFFER_SIZE);
             lastDataSent = millis();
             prevWeight = weight;
         }
@@ -104,32 +104,35 @@ void publishWeight(float weight)
 void publishRFID()
 {
 
-    if (rfid.isCard())
+    rfidtag = rfid.readTag();
+    rfid.printTag(rfidtag);
+
+    if (rfidtag != "None")
     {
-        rfidtag = rfid.readTag();
-        rfid.printTag(rfidtag);
         lcd.setCursor(0, 0);
-        lcd.print("Huidige RFID:");
-        lcd.setCursor(0, 1);
         lcd.print(rfidtag);
-        if (millis() - lastDataSent > 5000)
-        {
-            rfidtag.toCharArray(rfid_buffer, 11);
-            // client.publish("home/catbert/scales/Scale001/currentRFID", rfid_buffer);
-            lastDataSent = 0;
-        }
+        lcd.setCursor(1, 1);
+        lcd.print(rfidtag);
+        delay(500);
+    }
+
+    if (millis() - lastDataSent > 5000)
+    {
+        rfidtag.toCharArray(rfid_buffer, 11);
+        client.publish("home/catbert/scales/Scale001/currentRFID", rfid_buffer);
+        lastDataSent = 0;
     }
 }
 
-// boolean connect()
-// {
-//     Serial.println("Connecting to MQTT");
-//     if (client.connect("client"))
-//     {
-//         client.publish("home/catbert/scales/Scale001/status", "\"online\"");
-//     }
-//     return client.connected();
-// }
+boolean connect()
+{
+    Serial.println("Connecting to MQTT");
+    if (client.connect("client"))
+    {
+        client.publish("home/catbert/scales/Scale001/status", "\"online\"");
+    }
+    return client.connected();
+}
 
 void setup()
 {
@@ -137,30 +140,30 @@ void setup()
     Serial.println("Starting..");
 
     // Ethernet
-    // if (Ethernet.begin(mac) == 0)
-    // {
-    //     Serial.println(F("Unable to configure Ethernet using DHCP"));
-    //     for (;;)
-    //         ;
-    // }
+    if (Ethernet.begin(mac) == 0)
+    {
+        Serial.println(F("Unable to configure Ethernet using DHCP"));
+        for (;;)
+            ;
+    }
 
-    // if (!client.connected())
-    // {
-    //     if (connect())
-    //     {
-    //         lastReconnectAttempt = 0;
-    //     }
-    // }
+    if (!client.connected())
+    {
+        if (connect())
+        {
+            lastReconnectAttempt = 0;
+        }
+    }
 
-    // Serial.println(F("Ethernet configured via DHCP"));
-    // Serial.print("IP address: ");
-    // Serial.println(Ethernet.localIP());
+    Serial.println(F("Ethernet configured via DHCP"));
+    Serial.print("IP address: ");
+    Serial.println(Ethernet.localIP());
 
     // Cleanup any previous commands
 
     // Scale
-    // scale.set_scale(calibration_factor);
-    // scale.tare();
+    scale.set_scale(calibration_factor);
+    scale.tare();
 
     // RFID
     rfid.init();
@@ -169,8 +172,9 @@ void setup()
     lcd.begin(16, 2); // 16 characters, 2 rows
     lcd.setCursor(0, 0);
     lcd.print("IP: " + Ethernet.localIP());
+    delay(500);
 
-    // client.setServer(mqtt_server, 1883);
+    client.setServer(mqtt_server, 1883);
 
     Serial.println("Setup done");
 }
@@ -178,32 +182,32 @@ void setup()
 void loop()
 {
 
-    // if (!client.connected())
-    // {
-    //     Serial.println("MQTT not connected!");
+    if (!client.connected())
+    {
+        Serial.println("MQTT not connected!");
 
-    //     if (millis() - lastReconnectAttempt > REFRESH_INTERVAL)
-    //     {
+        if (millis() - lastReconnectAttempt > REFRESH_INTERVAL)
+        {
 
-    //         if (connect())
-    //         {
-    //             lastReconnectAttempt += REFRESH_INTERVAL;
-    //         }
-    //     }
-    // }
-    // else
-    // {
-    // client.loop();
+            if (connect())
+            {
+                lastReconnectAttempt += REFRESH_INTERVAL;
+            }
+        }
+    }
+    else
+    {
+        client.loop();
 
-    // double currentWeight = getWeight();
-    //  publishWeight(currentWeight);
+        double currentWeight = getWeight();
+        publishWeight(currentWeight);
 
-    publishRFID();
+        publishRFID();
 
-    // Subscribe to command topic to check if there are tasks to be done.
+        // Subscribe to command topic to check if there are tasks to be done.
 
-    // client.setKeepAlive(1000);
-    // }
+        client.setKeepAlive(1000);
+    }
 
     delay(500);
 }
